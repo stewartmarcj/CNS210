efaae90a811823907743fe6f14261695821b1032
stewartmarcj <62781173+stewartmarcj@users.noreply.github.com>
2020-05-02 10:12:50 -0400

    Add files via upload

---
+++ Parsingweb.py
@@ -0,0 +1,22 @@
+#Assignment 2 Parsing a website
+
+from bs4 import BeautifulSoup
+import requests
+import urllib
+
+page = requests.get('http://www.python.org/downloads')
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+download = soup.find_all("release-date", 'April 6, 2013')
+
+
+data = {}
+
+print("Begin Download")
+
+urllib.urlretrieve("https://www.python.org/downloads/release/python-274/", Marc-Python-247)
+
+print("Completed")
+
+
